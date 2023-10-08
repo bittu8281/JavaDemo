e39634e05f37d342f3b7771a4dc8b630e71cd01a
bittu8281 <53931871+bittu8281@users.noreply.github.com>
2023-10-08 22:36:51 +0530

    Add files via upload

---
+++ EvenOddNumber.java
@@ -0,0 +1,18 @@
+package ProgrammingCode;
+
+public class EvenOddNumber {
+
+	public static void main(String[] args) {
+		 int number = 25;
+		
+				 for(int n=1;n<=number;n++){
+			    if(n%2==0){
+			       System.out.println("This is even number = " + n);
+			    }
+			    else{
+			        System.out.println( "This is odd number = " + n);
+			    }
+			 }
+	}
+
+}
